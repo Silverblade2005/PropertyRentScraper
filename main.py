aa386e22c31ec9ccc7aa82d0d25adfc95c56f5f4
Silverblade2005 <63549786+Silverblade2005@users.noreply.github.com>
2023-12-11 12:42:06 +0200

    Get property suburb as test

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-province = input("Enter a province: ")
-city = input("Enter a city: ")
-bedroom_amt = input("Enter a bedroom amount: ")
-bathroom_amt = input("Enter a bathroom amount: ")
+province = "limpopo"#input("Enter a province: ")
+city = "polokwane"#input("Enter a city: ")
+bedroom_amt = "2"#input("Enter a bedroom amount: ")
+bathroom_amt = "1"#input("Enter a bathroom amount: ")
 page = 1
 
 url = f"https://www.remax.co.za/property/to-rent/south-africa/{province}/{city}/?bd={bedroom_amt}&bh={bathroom_amt}&page={page}"
@@ -14,4 +14,9 @@ page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
 
 listings_raw = soup.find_all('div', class_ ="property-card-info")
-print(listings_raw)
+
+for listing in listings_raw:
+    print("_________________________________________________________________________________")
+    #print(listing.prettify())
+    print(listing.find("div", class_="property-card-info__suburb").text)
+    print("_________________________________________________________________________________")
