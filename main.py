7e8a586fcadc6504a557a660420f828e652d79d0
Silverblade2005 <63549786+Silverblade2005@users.noreply.github.com>
2023-12-11 16:30:04 +0200

    Extract data ready to add into dataframe

--- main.py
+++ main.py
@@ -15,8 +15,28 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 listings_raw = soup.find_all('div', class_ ="property-card-info")
 
+table_headers = [ "Title", "Num_Rooms", "Num_Baths", "Property_Type" ,"Suburb" ,"Price"]
+table_data = []
+
+tmp_table_row = []
+
 for listing in listings_raw:
-    print("_________________________________________________________________________________")
-    #print(listing.prettify())
-    print(listing.find("div", class_="property-card-info__suburb").text)
-    print("_________________________________________________________________________________")
+    tmp_table_row = []
+    title = listing.find("a", class_="property-card-link")['title']
+    price = listing.find("span", attrs={"itemprop":"price"}).text.strip()
+    num_rooms = listing.find("span", attrs={"itemprop":"numberOfRooms"}).text
+    num_baths = listing.find("span", attrs={"itemprop":"amenityFeature"}).text
+    suburb_string = listing.find("div", class_="property-card-info__suburb").text
+    property_type = suburb_string.split(" ")[2]
+    suburb = suburb_string[suburb_string.find("in")+3:]
+
+    tmp_table_row.append(title)
+    tmp_table_row.append(num_rooms)
+    tmp_table_row.append(num_baths)
+    tmp_table_row.append(property_type)
+    tmp_table_row.append(suburb)
+    tmp_table_row.append(price)
+
+    table_data.append(tmp_table_row)
+
+print(table_data)
